459895a4c75dc0b459757666b426c0c53dcf06b0
Ravish-1 <86870933+Ravish-1@users.noreply.github.com>
2021-07-18 17:01:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var path,pathImage
 var jax,jaxImage
+var leftBoundary,rightBoundary,edges;
 
 function preload(){
   //pre-load images
@@ -13,21 +14,34 @@ function setup(){
   //create sprites here
   path=createSprite(300,300,40,40)
   path.addImage('path',pathImage)
-path.scale=1.5
+path.scale=1.2
 path.velocityY=4
 
 
 
-jax=createSprite(300,300,20,20)
+jax=createSprite(0,200,20,20)
 jax.addAnimation('jax',jaxImage)
 jax.scale=0.1
-  
+ 
+leftBoundary=createSprite(0,0,280,800)
+leftBoundary.visible=false;
+
+rightBoundary=createSprite(560,0,200,800)
+rightBoundary.visible=false;
+
+edges=createEdgeSprites();
 }
 
 function draw() {
   background('white');
-drawSprites()
-if(path.y>700){
+  jax.x=World.mouseX;
+
+
+if(path.y>450){
   path.y=height/2
 }
+jax.collide(leftBoundary);
+jax.collide(rightBoundary);
+jax.collide(edges[3])
+drawSprites()
 }
